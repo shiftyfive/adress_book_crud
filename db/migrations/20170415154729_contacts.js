811469de0c291c5a1b692e6606fd03a64c3fe83a
Joshua <joshua@JW-dev.local>
2017-04-16 17:52:25 -0700

    added routes added view index

--- db/migrations/20170415154729_contacts.js
+++ db/migrations/20170415154729_contacts.js
@@ -1,15 +1,14 @@
-exports.up = (knex) => {
-  return knex.schema.createTable('contacts', table => {
-    table.increments();
-    table.integer('address_id').notNullable();
-    table.string('first_name').notNullable();
-    table.string('last_name');
-    table.string('phone_number').notNullable();
-    table.string('email_address');
-    table.text('image_url');
-  });
-};
+function createContactsTable(table) {
+  table.increments();
+  table.integer('address_id').notNullable();
+  table.string('first_name').notNullable();
+  table.string('last_name');
+  table.string('phone_number').notNullable();
+  table.string('email_address');
+  table.text('image_url');
+}
 
-exports.down = (knex) => {
-  return knex.schema.dropTable('contacts');
-};
+
+exports.up = knex => knex.schema.createTable('contacts', createContactsTable);
+
+exports.down = knex => knex.schema.dropTable('contacts');
--- routes/contacts.js
+++ routes/contacts.js
@@ -4,13 +4,34 @@ const db = require('../db');
 const router = express.Router();
 
 
-/* GET users listing. */
+// get all contacts.
 router.get('/', (req, res) => {
   db.from('contacts').select('*')
-  .innerjoin('adresses', 'contacts.address_id', 'adresses.id')
+  .innerJoin('adresses', 'contacts.address_id', 'adresses.id')
   .then((contacts) => {
     res.render('contacts/index', { contacts });
   });
 });
+
+// route to add new contact.
+router.get('/contact/new', (req, res) => {
+  res.render('contact/form');
+});
+
+// route to edit exsisting contact.
+router.get('/id/edit', (req, res) => {
+  res.render('contact/form');
+});
+
+//render all contacts
+router.get('/id', (req, res) => {
+  const id = req.params.id;
+  db.from('contacts').where({ id }).first()
+  .innerJoin('adresses', 'contacts.adress_id', 'adresses.id')
+  .then((contact) => {
+    res.render('contact/show');
+  });
+});
+
 
 module.exports = router;
